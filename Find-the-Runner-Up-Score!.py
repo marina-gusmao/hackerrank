a22693424ff12e157aa08330c1e9fa2bbcdf6b8b
Marina Gusmão Faria Barbosa Bueno <marina.gusmao.bueno@alumni.usp.br>
2024-12-01 11:59:12 -0300

    Update and rename Find the Runner-Up Score! to Find-the-Runner-Up-Score!.py

--- Find-the-Runner-Up-Score!.py
+++ Find-the-Runner-Up-Score!.py
@@ -1,5 +1,9 @@
 # Find the Runner-Up Score!
 
+'''Given the participants' score sheet for your University Sports Day,'''
+'''you are required to find the runner-up score. You are given  scores.'''
+'''Store them in a list and find the score of the runner-up.'''
+
 n = int(input()) # number of scores
 s = input() # scores input as string (2 4 6 6 3)
 s = s.split() # it turns the string input into an iterable object
